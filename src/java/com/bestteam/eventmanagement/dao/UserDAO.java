16e3c5a1cd054a3857b17066a31e2aef945eeaa1
HoangDuong1106 <maihoangduong11062000@gmail.com>
2021-10-04 10:54:02 +0700

    Merge An vs Main

--- src/java/com/bestteam/eventmanagement/dao/UserDAO.java
+++ src/java/com/bestteam/eventmanagement/dao/UserDAO.java
@@ -6,10 +6,14 @@
 package com.bestteam.eventmanagement.dao;
 
 import com.bestteam.eventmanagement.dto.UserDTO;
+import com.bestteam.eventmanagement.utils.DBHelperUnitTest;
 import com.bestteam.eventmanagement.utils.DBHelper;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.naming.NamingException;
 
 /**
@@ -17,7 +21,6 @@ import javax.naming.NamingException;
  * @author admin
  */
 public class UserDAO {
-    
 
     //khi nguoi dung dang nhap bang mail fpt.edu.vn lan dau thi goi ham nay de insert thong tin vao DB
     public boolean insertNewUser(UserDTO user) throws SQLException, NamingException {
@@ -35,7 +38,7 @@ public class UserDAO {
                 stm.setString(3, user.getAvatar());
                 stm.setString(4, user.getAddress());
                 stm.setString(5, user.getPhoneNum());
-                stm.setInt(6, "STUDENT".equals(user.getRoleName())? 1 : 2);
+                stm.setInt(6, "STUDENT".equals(user.getRoleName()) ? 1 : 2);
                 check = stm.executeUpdate() > 0;
             }
         } finally {
@@ -49,9 +52,9 @@ public class UserDAO {
         return check;
 
     }
-    
+
     //Ham update thong tin user vao DB
-    public boolean updateUser(UserDTO user) throws SQLException {
+    public boolean updateUser(UserDTO user) throws SQLException, NamingException {
         boolean check = false;
         Connection conn = null;
         PreparedStatement stm = null;
@@ -68,8 +71,6 @@ public class UserDAO {
                 stm.setString(4, user.getEmail());
                 check = stm.executeUpdate() > 0;
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         } finally {
             if (stm != null) {
                 stm.close();
@@ -80,5 +81,141 @@ public class UserDAO {
         }
         return check;
     }
+
+    public boolean findUser(int userid) throws SQLException, ClassNotFoundException {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBHelperUnitTest.getConnection();
+            if (conn != null) {
+                String sql = "select email from tblUsers where id = ?";
+                stm = conn.prepareStatement(sql);
+                stm.setInt(1, userid);
+                rs = stm.executeQuery();
+                if (rs.next()) {
+                    check = true;
+                }
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return check;
+    }
+
+    public String getRoleName(int roleID) throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBHelperUnitTest.getConnection();
+            String sql = "select roleName from tblRoles where id  = ?";
+            stm = conn.prepareStatement(sql);
+            stm.setInt(1, roleID);
+            rs = stm.executeQuery();
+            if (rs.next()) {
+                return rs.getString(1);
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return null;
+    }
+
+    public UserDTO findUserReturnDTO(int userid) throws SQLException, ClassNotFoundException {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        UserDAO userDAO = new UserDAO();
+        try {
+            conn = DBHelperUnitTest.getConnection();
+            if (conn != null) {
+                String sql = "select * from tblUsers where id  = ?";
+                stm = conn.prepareStatement(sql);
+                stm.setInt(1, userid);
+                rs = stm.executeQuery();
+                if (rs.next()) {
+                    return new UserDTO(rs.getInt("id"),
+                            rs.getString("email"),
+                            rs.getString("name"),
+                            rs.getString("avatar"),
+                            rs.getString("address"),
+                            rs.getString("phoneNum"),
+                            userDAO.getRoleName(rs.getInt("roleId")));
+                }
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return null;
+    }
+
+    public List<UserDTO> getAllUser() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        List<UserDTO> arrayList = new ArrayList<>();
+        ResultSet rs = null;
+        UserDAO userDAO = new UserDAO();
+        try {
+            conn = DBHelperUnitTest.getConnection();
+            if (conn != null) {
+                String sql = "select * from tblUsers";
+                stm = conn.prepareStatement(sql);
+                rs = stm.executeQuery();
+                while (rs.next()) {
+                    arrayList.add(new UserDTO(rs.getInt("id"),
+                            rs.getString("email"),
+                            rs.getString("name"),
+                            rs.getString("avatar"),
+                            rs.getString("address"),
+                            rs.getString("phoneNum"),
+                            userDAO.getRoleName(rs.getInt("roleId"))));
+                }
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return arrayList;
+    }
+//    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        UserDAO userDAO = new UserDAO();
+//        for (UserDTO userDTO : userDAO.getAllUser()) {
+//            System.out.println(userDTO.toString());
+//        }
+//    }
 
 }
